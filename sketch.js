0f8e4664da3ecfdce1bf2812d5311c186aeb404a
Anuraag-Chakraborty <71392140+Anuraag-Chakraborty@users.noreply.github.com>
2020-11-26 16:53:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,23 +16,23 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	roofObject=new roof(width/2,height/4,width/7,20);
+	roofObject=new Roof(width/2,height/4,width/7,20);
 
 	bobDiameter=40;
 
 	startBobPositionX=width/2;
 	startBobPositionY=height/4+500;
-	bobObject1=new bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
-	bobObject2=new bob(startBobPositionX-bobDiameter,startBobPositionY,bobDiameter);
-	bobObject3=new bob(startBobPositionX,startBobPositionY,bobDiameter);
-	bobObject4=new bob(startBobPositionX+bobDiameter,startBobPositionY,bobDiameter);
-	bobObject5=new bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
+	bobObject1=new Bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
+	bobObject2=new Bob(startBobPositionX-bobDiameter,startBobPositionY,bobDiameter);
+	bobObject3=new Bob(startBobPositionX,startBobPositionY,bobDiameter);
+	bobObject4=new Bob(startBobPositionX+bobDiameter,startBobPositionY,bobDiameter);
+	bobObject5=new Bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
 	
-	rope1=new rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0)
-	rope2=new rope(bobObject2.body,roofObject.body,-bobDiameter*1, 0)
-	rope3=new rope(bobObject3.body,roofObject.body,0, 0)
-	rope4=new rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
-	rope5=new rope(bobObject5.body,roofObject.body,bobDiameter*2, 0)
+	rope1=new Rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0)
+	rope2=new Rope(bobObject2.body,roofObject.body,-bobDiameter*1, 0)
+	rope3=new Rope(bobObject3.body,roofObject.body,0, 0)
+	rope4=new Rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
+	rope5=new Rope(bobObject5.body,roofObject.body,bobDiameter*2, 0)
 
 	Engine.run(engine);
   
