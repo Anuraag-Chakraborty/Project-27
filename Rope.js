35b99c8c7a501fd4e29e83577ce5fc7dd825632d
Anuraag-Chakraborty <71392140+Anuraag-Chakraborty@users.noreply.github.com>
2020-11-26 16:59:40 +0530

    Update Rope.js

--- Rope.js
+++ Rope.js
@@ -17,9 +17,12 @@ class Rope {
      var pointB = this.rope.bodyB.position
      strokeWeight(2);
 
-     var Anchor2X = pointB.x + this.offsetX
-     var Anchor2Y = pointB.y + this.offsetY
+    var Anchor1X=pointA.x
+		var Anchor1Y=pointA.y
 
-     line(Anchor1X, Anchor1Y, Anchor2X, Anchor2Y);
+		var Anchor2X=pointB.x+this.offsetX
+		var Anchor2Y=pointB.y+this.offsetY
+
+		line(Anchor1X,Anchor1Y,Anchor2X,Anchor2Y);
     }
 }
